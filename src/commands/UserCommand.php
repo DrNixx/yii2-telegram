c61766f673ce61ff015cd5d41849818f530d83f4
DrNixx <drnixx@gmail.com>
2020-09-09 14:27:45 +0300

    Add comand restriction for not linked users

--- src/commands/UserCommand.php
+++ src/commands/UserCommand.php
@@ -7,4 +7,21 @@ abstract class UserCommand extends Command
      * @inheritdoc
      */
     protected $category = 'User';
+
+    /**
+     * Make sure this command only executes for linked users.
+     *
+     * @var bool
+     */
+    protected $linked_only = false;
+
+    /**
+     * If this command is intended for linked users only.
+     *
+     * @return bool
+     */
+    public function isLinkedOnly()
+    {
+        return $this->linked_only;
+    }
 }
