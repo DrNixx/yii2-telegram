17a5eb9c3a05fff382f116a0fa005c5bc478aed3
DrNixx <drnixx@gmail.com>
2023-08-10 14:57:24 +0300

    Bot API 5.6

--- src/entities/MessageEntity.php
+++ src/entities/MessageEntity.php
@@ -6,11 +6,13 @@ namespace onix\telegram\entities;
  *
  * @link https://core.telegram.org/bots/api#messageentity
  *
- * @property-read string $type Type of the entity. Can be 'mention' (@username), 'hashtag' (#hashtag), 'cashtag' ($USD),
- * 'bot_command' (/start@jobs_bot), 'url' (https://telegram.org), 'email' (do-not-reply@telegram.org),
- * 'phone_number' (+1-212-555-0123), 'bold' (bold text), 'italic' (italic text), 'underline' (underlined text),
- * 'strikethrough' (strikethrough text), 'code' (monowidth string), 'pre' (monowidth block), 'text_link'
- * (for clickable text URLs), 'text_mention' (for users without usernames)
+ * @property-read string $type Type of the entity. Currently, can be “mention” (@username), “hashtag” (#hashtag),
+ * “cashtag” ($USD), “bot_command” (/start@jobs_bot), “url” (https://telegram.org),
+ * “email” (do-not-reply@telegram.org), “phone_number” (+1-212-555-0123), “bold” (bold text),
+ * “italic” (italic text), “underline” (underlined text), “strikethrough” (strikethrough text),
+ * “spoiler” (spoiler message), “code” (monowidth string), “pre” (monowidth block),
+ * “text_link” (for clickable text URLs), “text_mention” (for users without usernames),
+ * “custom_emoji” (for inline custom emoji stickers)
  *
  * @property-read int $offset Offset in UTF-16 code units to the start of the entity
  * @property-read int $length Length of the entity in UTF-16 code units
